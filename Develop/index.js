60875f8cf631af98cf0b730c9a5add858f555587
Marcia Paige <mapaige7@gmail.com>
2020-08-15 18:57:52 -0400

    Added inquirers

--- Develop/index.js
+++ Develop/index.js
@@ -14,3 +14,49 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+var inquirer = require("inquirer");
+inquirer
+.prompt([
+  {
+    type: "input",
+    message: "Title of the project:",
+    name: "title"
+  },
+  {
+   type:"input",
+   message: "Description",
+   title: "description"
+  },
+  {
+    type: "input",
+    message: "Table of Contents",
+    title: "TOC"
+  },
+  {
+    type: "input",
+    message: "Installation",
+    title: "install"
+  },
+  {
+    type: "input",
+    message: "Usage",
+    title:"usage"
+  },
+  {
+    type: "input",
+    message: "License",
+    title: "license"
+  },
+  {
+    type :"input",
+    message: "Contributing",
+    title :"contributors"
+  },
+  {
+    type: "input",
+    message: "Questions",
+    title : "questions"
+  }
+])
